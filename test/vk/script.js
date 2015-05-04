a2e66837e9a73b32a6b638cf73b550a1401f8255
Alexey <avaelkross@gmail.com>
2015-05-04 02:22:47 +0200

    ???

--- test/vk/script.js
+++ test/vk/script.js
@@ -1,3 +1,23 @@
 $(document).ready(function() {
-  $("#vk_id").html("?");
+
+  function authInfo(response) {    
+      if(response.status=='connected') {  // если пользователь залогинен в ВК     
+          if(typeof(response.session.user) == 'undefined') { // этого поля нет 
+                  //тогда, когда пользователь был залогинен ранее
+                  VK.Api.call('users.get', { uid: response.session.mid }, function(r) { 
+                      $('#vk_id').html(r.response[0].first_name+' '+r.response[0].last_name); 
+                      $('#vk_id').append('user_id: '+response.session.mid); });
+     }else {
+          // если авторизация прошла только что (от VK.Auth.login(authInfo);), то имя и фамилия уже будут в ответе   
+          $('#vk_id').html(response.session.user.first_name+' '+response.session.user.last_name);
+          $('#vk_id').append('
+  user_id: '+response.session.mid);}
+     
+         }else {
+          VK.Auth.login(authInfo); // опционально можем спалиться и вызвать всплывающее окно авторизации
+      }
+  }
+
+  $(function() { VK.Auth.getLoginStatus(authInfo); }); // проверяем наличие входа в ВК, ответ отправляем в функцию обработчик
+
 });
